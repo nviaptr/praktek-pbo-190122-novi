63ffad9b232627edfa48f719c943d4addc2ccae4
novi <novia@gmail.com>
2022-01-24 09:57:32 +0700

    tugas novi

---
+++ barisan-bintang/barisanBintang.js
@@ -0,0 +1,9 @@
+let baris = 6;
+let star = ""
+for (let i=0 ; i <baris; i++) {
+        console.log("Loop 1", i);
+        for(let j=0; j<4; j++) {
+        console.log("Loop 2", j);
+        }
+}
+
