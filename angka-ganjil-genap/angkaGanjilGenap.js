b86b90c62df5de22e52c050b4dc696c1d9278bc6
novi <novia@gmail.com>
2022-01-24 08:12:32 +0700

    tugas novi

--- angka-ganjil-genap/angkaGanjilGenap.js
+++ angka-ganjil-genap/angkaGanjilGenap.js
@@ -5,3 +5,20 @@ for (let i= 1; i <= 100; i+=3) {
         console.log(i +"- ganjil");
     }
 }
+
+for (let i= 50; i <= 200; i+=5) {
+    if (i % 3===0) {
+        console.log(i + "- habis dibagi tiga");
+    }
+}
+
+for (let i= 100; i <= 200; i+=7) {
+    if (i % 8===0) {
+        console.log(i + "- habis dibagi delapan");
+    }
+}
+
+
+
+
+
